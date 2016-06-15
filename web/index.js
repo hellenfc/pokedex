5699c30d3b0e4b07948c9820739298f1199b2f3c
Hellen <hellenfc93@gmail.com>
2016-06-15 02:14:00 -0600

    Schema

---
+++ web/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var app = express();
+//To grab data from POST
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use(express.static(__dirname));
+//Setting up morgan to log every request
+app.use(morgan('dev'));
+//Connecting to DB
+mongoose.connect(config.DATABASE);
+//Root of webpage
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+//Endpoints
+
+//Listen
+var server = app.listen(config.PORT, function(){
+  console.log('Server Running on: ' + config.PORT);
+});
